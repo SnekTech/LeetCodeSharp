f4d99cd6c9550deaae48ecb9353c27f1c4e18c27
syf <2654525303@qq.com>
2024-03-23 22:16:17 +0800

    169

---
+++ Interview150/169/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace Interview150._169;
+
+public class Solution {
+    public int MajorityElement(int[] nums)
+    {
+        var numCounts = CountNums(nums);
+
+        int candidate = nums[0], candidateCount = 1;
+        foreach (var (num, count) in numCounts)
+        {
+            if (count > candidateCount)
+            {
+                candidate = num;
+                candidateCount = count;
+            }
+        }
+
+        return candidate;
+    }
+
+    private static Dictionary<int, int> CountNums(int[] nums)
+    {
+        var dict = new Dictionary<int, int>();
+        foreach (var num in nums)
+        {
+            if (!dict.TryAdd(num, 1))
+            {
+                dict[num] += 1;
+            }
+        }
+
+        return dict;
+    }
+}
