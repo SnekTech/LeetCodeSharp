38e5535c9dd29e88502d6a6c5bcd19a16e262c44
syf <2654525303@qq.com>
2024-03-23 17:41:19 +0800

    init

---
+++ Classic150/88/Solution.cs
@@ -0,0 +1,32 @@
+﻿namespace Classic150._88;
+
+public class Solution
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        int p1 = m - 1, p2 = n - 1, tail = m + n - 1;
+
+        while (p1 >= 0 || p2 >= 0)
+        {
+            int current;
+            if (p1 == -1)
+            {
+                current = nums2[p2--];
+            }
+            else if (p2 == -1)
+            {
+                current = nums1[p1--];
+            }
+            else if (nums1[p1] > nums2[p2])
+            {
+                current = nums1[p1--];
+            }
+            else
+            {
+                current = nums2[p2--];
+            }
+
+            nums1[tail--] = current;
+        }
+    }
+}
---
+++ Classic150/88/Test.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Classic150._88;
+
+public class Test
+{
+    [Test]
+    public void Case1()
+    {
+        var solution = new Solution();
+        int[] nums1 = [0], nums2 = [1];
+        const int m = 0, n = 1;
+        solution.Merge(nums1, m, nums2, n);
+
+        nums1.Should().Equal([1]);
+    }
+}
