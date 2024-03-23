36da4aaf86bb6100daf4fd4d0adaf57c91d034a1
syf <2654525303@qq.com>
2024-03-23 18:01:09 +0800

    27

---
+++ Classic150/27/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace Classic150._27;
+
+public class Solution {
+    public int RemoveElement(int[] nums, int val)
+    {
+        int n = nums.Length;
+        int left = 0;
+        for (int right = 0; right < n; right++)
+        {
+            if (nums[right] != val)
+            {
+                nums[left] = nums[right];
+                left++;
+            }
+        }
+
+        return left;
+    }
+}
