8d4ecd6843a475930b38303841702d47d00d61bf
syf <2654525303@qq.com>
2024-03-23 18:08:25 +0800

    26

---
+++ Classic150/26/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace Classic150._26;
+
+public class Solution {
+    public int RemoveDuplicates(int[] nums)
+    {
+        int n = nums.Length;
+        if (n == 0) return 0;
+
+        int slow = 1, fast = 1;
+        while (fast < n)
+        {
+            if (nums[fast] != nums[fast - 1])
+            {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+
+            fast++;
+        }
+
+        return slow;
+    }
+}
