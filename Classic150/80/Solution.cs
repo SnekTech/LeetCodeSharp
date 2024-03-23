cf160503bb80ccb7a4348daf0d635382c0fe5797
syf <2654525303@qq.com>
2024-03-23 21:47:44 +0800

    80

---
+++ Classic150/80/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace Classic150._80;
+
+public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        int n = nums.Length;
+        if (n <= 2)
+            return n;
+
+        int slow = 2, fast = 2;
+        while (fast < n)
+        {
+            if (nums[fast] != nums[slow - 2])
+            {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+
+            fast++;
+        }
+
+        return slow;
+    }
+}
---
+++ Classic150/80/Test.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Classic150._80;
+
+public class Test
+{
+    [Test]
+    public void Case1()
+    {
+        var solution = new Solution();
+        int[] nums = [1, 1, 1, 2, 2, 3];
+        int[] expected = [1, 1, 2, 2, 3];
+
+        int n = solution.RemoveDuplicates(nums);
+        nums[0..n].Should().Equal(expected);
+    }
+}
