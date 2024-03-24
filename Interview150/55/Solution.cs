2f45e3e195fa181633db0012a823c5c40bcd2d25
syf <2654525303@qq.com>
2024-03-24 17:30:55 +0800

    55

---
+++ Interview150/55/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace Interview150._55;
+
+public class Solution
+{
+    public bool CanJump(int[] nums)
+    {
+        int n = nums.Length;
+        int right = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (right < i) return false;
+            right = Math.Max(right, i + nums[i]);
+        }
+
+        return true;
+    }
+}
