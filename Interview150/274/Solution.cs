4cac85a35852305a2c6510d83c4656640365342a
syf <2654525303@qq.com>
2024-03-24 17:49:20 +0800

    274

---
+++ Interview150/274/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace Interview150._274;
+
+public class Solution
+{
+    public int HIndex(int[] citations)
+    {
+        int n = citations.Length, total = 0;
+        int[] count = new int[n + 1];
+
+        for (int i = 0; i < n; i++)
+        {
+            if (citations[i] >= n)
+            {
+                count[n]++;
+            }
+            else
+            {
+                count[citations[i]]++;
+            }
+        }
+
+        for (int i = n; i >= 0; i--)
+        {
+            total += count[i];
+            if (total >= i)
+            {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+}
