2ee66b5c5ecb1432ebf4041801bad0727d391bc4
syf <2654525303@qq.com>
2024-03-24 18:00:27 +0800

    380

---
+++ Interview150/380/RandomizedSet.cs
@@ -0,0 +1,43 @@
+﻿namespace Interview150._380;
+
+public class RandomizedSet
+{
+    private IList<int> nums = new List<int>();
+    private Dictionary<int, int> indices = new();
+    private Random random = new();
+
+    public bool Insert(int val)
+    {
+        if (indices.ContainsKey(val))
+        {
+            return false;
+        }
+
+        int index = nums.Count;
+        nums.Add(val);
+        indices.Add(val, index);
+        return true;
+    }
+
+    public bool Remove(int val)
+    {
+        if (!indices.ContainsKey(val))
+        {
+            return false;
+        }
+
+        int index = indices[val];
+        int last = nums.Last();
+        nums[index] = last;
+        indices[last] = index;
+        nums.RemoveAt(nums.Count - 1);
+        indices.Remove(val);
+        return true;
+    }
+
+    public int GetRandom()
+    {
+        int randomIndex = random.Next(nums.Count);
+        return nums[randomIndex];
+    }
+}
