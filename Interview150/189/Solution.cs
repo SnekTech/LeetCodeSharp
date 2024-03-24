d9b0dde26fa7ede8ac90a063b0dd8acbeb5b7b7d
syf <2654525303@qq.com>
2024-03-24 12:04:01 +0800

    189

---
+++ Interview150/189/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace Interview150._189;
+
+public class Solution
+{
+    public void Rotate(int[] nums, int k)
+    {
+        int n = nums.Length;
+        var rotated = new List<int>(n);
+        rotated.AddRange(nums);
+        for (int i = 0; i < n; i++)
+        {
+            rotated[(i + k) % n] = nums[i];
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            nums[i] = rotated[i];
+        }
+    }
+}
