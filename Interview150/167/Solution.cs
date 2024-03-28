194dc14d0d86675af3a604e9eedecd0cd11a972a
syf <2654525303@qq.com>
2024-03-28 21:49:37 +0800

    167

---
+++ Interview150/167/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace Interview150._167;
+
+public class Solution
+{
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        int low = 0, high = numbers.Length - 1;
+        while (low < high)
+        {
+            int sum = numbers[low] + numbers[high];
+            if (sum == target)
+            {
+                return [low + 1, high + 1];
+            }
+            else if (sum < target)
+            {
+                low++;
+            }
+            else
+            {
+                high--;
+            }
+        }
+
+        return [-1, -1];
+    }
+}
