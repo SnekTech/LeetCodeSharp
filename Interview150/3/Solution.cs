ed287789b5ea532988dbe8ac297de0f9bf375f95
syf <2654525303@qq.com>
2024-03-28 18:01:51 +0800

    3

---
+++ Interview150/3/Solution.cs
@@ -0,0 +1,38 @@
+﻿namespace Interview150._3;
+
+public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        HashSet<char> uniqueLetters = [];
+        int n = s.Length;
+        if (n <= 1) return n;
+
+        int left = 0, right = 1, maxLen = 1;
+        uniqueLetters.Add(s[left]);
+
+        while (right < n)
+        {
+            var c = s[right];
+            if (!uniqueLetters.Contains(c))
+            {
+                uniqueLetters.Add(c);
+                maxLen = Math.Max(maxLen, right - left + 1);
+            }
+            else
+            {
+                while (s[left] != c)
+                {
+                    uniqueLetters.Remove(s[left]);
+                    left++;
+                }
+
+                left++;
+            }
+
+            right++;
+        }
+
+        return maxLen;
+    }
+}
