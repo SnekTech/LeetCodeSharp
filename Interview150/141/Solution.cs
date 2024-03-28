c2f8f05231b183bef7aa9b22fea7e5539bc50a2c
syf <2654525303@qq.com>
2024-03-28 21:56:12 +0800

    141

---
+++ Interview150/141/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace Interview150._141;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool HasCycle(ListNode head)
+    {
+        var nodes = new HashSet<ListNode>();
+        var p = head;
+
+        while (p != null)
+        {
+            if (nodes.Contains(p))
+            {
+                return true;
+            }
+
+            nodes.Add(p);
+            p = p.next;
+        }
+
+        return false;
+    }
+}
