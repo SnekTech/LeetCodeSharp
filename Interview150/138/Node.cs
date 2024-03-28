2fa4a397c371040ce1abbd11487604206630ebe3
syf <2654525303@qq.com>
2024-03-28 22:37:14 +0800

    138

---
+++ Interview150/138/Node.cs
@@ -0,0 +1,15 @@
+﻿namespace Interview150._138;
+
+public class Node
+{
+    public int val;
+    public Node next;
+    public Node random;
+
+    public Node(int _val)
+    {
+        val = _val;
+        next = null;
+        random = null;
+    }
+}
---
+++ Interview150/138/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace Interview150._138;
+
+public class Solution {
+    public Node CopyRandomList(Node head)
+    {
+        var originToReplicate = new Dictionary<Node, Node>();
+        Node p = head, dummy = new Node(-1), prev = dummy;
+        while (p != null)
+        {
+            var replicate = new Node(p.val);
+            originToReplicate.Add(p, replicate);
+            prev.next = replicate;
+            prev = prev.next;
+            p = p.next;
+        }
+
+        p = head;
+        while (p != null)
+        {
+            var replicate = originToReplicate[p];
+            replicate.random = p.random != null ? originToReplicate[p.random] : null;
+            p = p.next;
+        }
+
+        return dummy.next;
+    }
+}
