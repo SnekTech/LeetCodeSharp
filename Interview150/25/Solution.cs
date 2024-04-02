41f5292318bc7356be60ed7a9f7ac3ab507a86e7
syf <2654525303@qq.com>
2024-04-02 15:03:29 +0800

    25

---
+++ Interview150/25/Solution.cs
@@ -0,0 +1,56 @@
+﻿using Interview150._2;
+
+namespace Interview150._25;
+
+public class Solution
+{
+    public ListNode ReverseKGroup(ListNode head, int k)
+    {
+        var dummy = new ListNode(-1, head);
+        var prev = dummy;
+        var begin = head;
+        var end = head;
+
+        while (true)
+        {
+            var shouldReverse = true;
+            for (int i = 0; i < k; i++)
+            {
+                if (end == null)
+                {
+                    shouldReverse = false;
+                    break;
+                }
+
+                end = end.next;
+            }
+
+            if (!shouldReverse)
+            {
+                prev.next = begin;
+                break;
+            }
+
+            prev.next = Reverse(begin, end);
+            prev = begin;
+            begin = end;
+        }
+
+        return dummy.next;
+    }
+
+    private ListNode Reverse(ListNode head, ListNode end)
+    {
+        var dummy = new ListNode(-1);
+        var p = head;
+        while (p != end)
+        {
+            var nextP = p.next;
+            p.next = dummy.next;
+            dummy.next = p;
+            p = nextP;
+        }
+
+        return dummy.next;
+    }
+}
