91c61b6080bbab0c3319a76ed0af7051a878c2ab
syf <2654525303@qq.com>
2024-04-02 16:40:40 +0800

    19

---
+++ Interview150/19/Solution.cs
@@ -0,0 +1,27 @@
+﻿using Interview150._2;
+
+namespace Interview150._19;
+
+public class Solution
+{
+    public ListNode RemoveNthFromEnd(ListNode head, int n)
+    {
+        var dummy = new ListNode(-1, head);
+        var prev = dummy;
+        var end = head;
+        for (int i = 0; i < n; i++)
+        {
+            end = end.next;
+        }
+
+        while (end != null)
+        {
+            prev = prev.next;
+            end = end.next;
+        }
+
+        prev.next = prev.next.next;
+
+        return dummy.next;
+    }
+}
