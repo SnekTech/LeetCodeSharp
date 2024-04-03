81373ee809a44b44ecede27aacab50ab60342556
syf <2654525303@qq.com>
2024-04-03 18:08:00 +0800

    61

---
+++ Interview150/61/Solution.cs
@@ -0,0 +1,39 @@
+﻿using Interview150._2;
+
+namespace Interview150._61;
+
+public class Solution
+{
+    public ListNode RotateRight(ListNode head, int k)
+    {
+        if (k == 0 || head == null || head.next == null)
+        {
+            return head;
+        }
+
+        var dummy = new ListNode(-1, head);
+        var prev = dummy;
+        var n = 0;
+        while (prev.next != null)
+        {
+            n++;
+            prev = prev.next;
+        }
+
+        // prev references tail now,
+
+        var offset = n - k % n;
+        if (offset == n) return head;
+        
+        // build circle
+        prev.next = head;
+        while (offset-- > 0)
+        {
+            prev = prev.next;
+        }
+
+        var newHead = prev.next;
+        prev.next = null;
+        return newHead;
+    }
+}
