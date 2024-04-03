88431b0db8c7920dfb59649a61ccf94af7facc31
syf <2654525303@qq.com>
2024-04-03 17:48:21 +0800

    82

---
+++ Interview150/82/Solution.cs
@@ -0,0 +1,40 @@
+﻿using Interview150._2;
+
+namespace Interview150._82;
+
+public class Solution
+{
+    public ListNode DeleteDuplicates(ListNode head)
+    {
+        var dummy = new ListNode(-101, head);
+        var prev = dummy;
+        var p = head;
+        int sameVal = -1;
+
+        while (p != null)
+        {
+            if (p.next == null)
+            {
+                break;
+            }
+
+            sameVal = p.val;
+            if (p.val == p.next.val)
+            {
+                while (p != null && p.val == sameVal)
+                {
+                    p = p.next;
+                }
+
+                prev.next = p;
+            }
+            else
+            {
+                prev = p;
+                p = p.next;
+            }
+        }
+
+        return dummy.next;
+    }
+}
