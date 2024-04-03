da025e8e444ac4109e5214c7176d12672ec3dbb8
syf <2654525303@qq.com>
2024-04-03 23:03:20 +0800

    104

---
+++ Interview150/104/Solution.cs
@@ -0,0 +1,14 @@
+﻿using Interview150.common;
+
+namespace Interview150._104;
+
+public class Solution
+{
+    public int MaxDepth(TreeNode root)
+    {
+        if (root == null)
+            return 0;
+
+        return Math.Max(MaxDepth(root.left), MaxDepth(root.right)) + 1;
+    }
+}
---
+++ Interview150/common/TreeNode.cs
@@ -0,0 +1,7 @@
+﻿namespace Interview150.common;
+
+public class TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+{
+    public int val = val;
+    public TreeNode left = left, right = right;
+}
