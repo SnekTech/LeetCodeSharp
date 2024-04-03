f59964657b94660f3ae9b761ac1243049065c517
syf <2654525303@qq.com>
2024-04-03 23:08:54 +0800

    100

---
+++ Interview150/100/Solution.cs
@@ -0,0 +1,19 @@
+﻿using Interview150.common;
+
+namespace Interview150._100;
+
+public class Solution
+{
+    public bool IsSameTree(TreeNode p, TreeNode q)
+    {
+        if (p == q)
+            return true;
+        if (p == null || q == null)
+            return false;
+
+        if (p.val != q.val)
+            return false;
+
+        return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+    }
+}
