7361026e5730147ed59c8cf1fce6768a7b6ac3ec
syf <2654525303@qq.com>
2024-04-03 23:13:26 +0800

    226

---
+++ Interview150/226/Solution.cs
@@ -0,0 +1,19 @@
+﻿using Interview150.common;
+
+namespace Interview150._226;
+
+public class Solution
+{
+    public TreeNode InvertTree(TreeNode root)
+    {
+        if (root == null)
+            return null;
+
+        var left = root.left;
+        var right = root.right;
+        root.left = InvertTree(right);
+        root.right = InvertTree(left);
+
+        return root;
+    }
+}
