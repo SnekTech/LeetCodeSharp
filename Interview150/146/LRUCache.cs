0cbdd94c414e84053b9f754f83d9c3aa53544d6f
syf <2654525303@qq.com>
2024-04-03 22:59:43 +0800

    146

---
+++ Interview150/146/LRUCache.cs
@@ -0,0 +1,94 @@
+﻿namespace Interview150._146;
+
+public class LRUCache
+{
+    private class DLinkNode(int key = -1, int value = -1)
+    {
+        public DLinkNode prev = null;
+        public DLinkNode next = null;
+        public int key = key;
+        public int val = value;
+    }
+
+    private Dictionary<int, DLinkNode> nodeByKey;
+    private DLinkNode dummyHead;
+    private DLinkNode dummyTail;
+    private int size = 0;
+    private readonly int capacity;
+
+    public LRUCache(int capacity)
+    {
+        nodeByKey = new Dictionary<int, DLinkNode>(capacity);
+        this.capacity = capacity;
+
+        dummyHead = new DLinkNode();
+        dummyTail = new DLinkNode();
+        dummyHead.next = dummyTail;
+        dummyTail.prev = dummyHead;
+    }
+
+    public int Get(int key)
+    {
+        if (!nodeByKey.ContainsKey(key))
+        {
+            return -1;
+        }
+
+        // find node by key,
+        // then move the node to head
+        var targetNode = nodeByKey[key];
+        MoveToHead(targetNode);
+
+        return targetNode.val;
+    }
+
+    public void Put(int key, int value)
+    {
+        if (nodeByKey.ContainsKey(key))
+        {
+            var node = nodeByKey[key];
+            node.val = value;
+            MoveToHead(node);
+        }
+        else
+        {
+            var newNode = new DLinkNode(key, value);
+            nodeByKey[key] = newNode;
+            AddToHead(newNode);
+            size++;
+            if (size > capacity)
+            {
+                var tail = RemoveTail();
+                nodeByKey.Remove(tail.key);
+                size--;
+            }
+        }
+    }
+
+    private void MoveToHead(DLinkNode node)
+    {
+        RemoveNode(node);
+        AddToHead(node);
+    }
+
+    private void AddToHead(DLinkNode node)
+    {
+        node.prev = dummyHead;
+        node.next = dummyHead.next;
+        dummyHead.next.prev = node;
+        dummyHead.next = node;
+    }
+
+    private void RemoveNode(DLinkNode node)
+    {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private DLinkNode RemoveTail()
+    {
+        var tail = dummyTail.prev;
+        RemoveNode(tail);
+        return tail;
+    }
+}
---
+++ Interview150/146/Test.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Interview150._146;
+
+[TestFixture]
+public class Test
+{
+    [Test]
+    public void Case1()
+    {
+        var lru = new LRUCache(2);
+        lru.Put(1, 1);
+        lru.Put(2, 2);
+        lru.Get(1).Should().Be(1);
+
+        lru.Put(3, 3);
+        lru.Get(2).Should().Be(-1);
+        lru.Put(4, 4);
+        lru.Get(1).Should().Be(-1);
+        lru.Get(3).Should().Be(3);
+        lru.Get(4).Should().Be(4);
+    }
+}
