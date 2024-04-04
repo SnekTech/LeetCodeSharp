a4aae271e22a380b247698f988e15714792094be
syf <2654525303@qq.com>
2024-04-04 16:39:15 +0800

    117

---
+++ Interview150/117/Solution.cs
@@ -0,0 +1,39 @@
+﻿namespace Interview150._117;
+
+public class Solution
+{
+    public Node Connect(Node root)
+    {
+        if (root == null)
+            return null;
+
+        var queue = new Queue<Node>();
+        queue.Enqueue(root);
+        while (queue.Count > 0)
+        {
+            var levelSize = queue.Count;
+            Node last = null;
+            for (var i = 0; i < levelSize; i++)
+            {
+                var node = queue.Dequeue();
+                if (node.left != null)
+                {
+                    queue.Enqueue(node.left);
+                }
+                if (node.right != null)
+                {
+                    queue.Enqueue(node.right);
+                }
+
+                if (last != null)
+                {
+                    last.next = node;
+                }
+
+                last = node;
+            }
+        }
+
+        return root;
+    }
+}
