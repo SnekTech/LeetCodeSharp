b3fc23838ac6420e65ba1bc6687730e6a4ca420e
syf <2654525303@qq.com>
2024-04-04 17:00:46 +0800

    114

---
+++ Interview150/114/Solution.cs
@@ -0,0 +1,41 @@
+﻿using Interview150.common;
+
+namespace Interview150._114;
+
+public class Solution
+{
+    public void Flatten(TreeNode root)
+    {
+        GetFlattened(root);
+    }
+
+    private (TreeNode head, TreeNode tail) GetFlattened(TreeNode root)
+    {
+        if (root == null)
+            return (null, null);
+
+        var (leftHead, leftTail) = GetFlattened(root.left);
+        var (rightHead, rightTail) = GetFlattened(root.right);
+        root.left = null;
+
+        if (leftHead == null && rightHead == null)
+            return (root, root);
+        
+        if (leftHead != null && rightHead != null)
+        {
+            root.right = leftHead;
+            leftTail.right = rightHead;
+            return (root, rightTail);
+        }
+
+        if (leftHead != null)
+        {
+            root.right = leftHead;
+            return (root, leftTail);
+        }
+
+        // leftHead == null && rightHead != null
+        root.right = rightHead;
+        return (root, rightTail);
+    }
+}
