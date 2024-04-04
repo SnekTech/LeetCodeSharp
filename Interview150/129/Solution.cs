484b3c66f2872c34833614c4957cb43376e825db
syf <2654525303@qq.com>
2024-04-04 23:38:05 +0800

    129

---
+++ Interview150/129/Solution.cs
@@ -0,0 +1,25 @@
+﻿using Interview150.common;
+
+namespace Interview150._129;
+
+public class Solution
+{
+    public int SumNumbers(TreeNode root)
+    {
+        return Dfs(root, 0);
+    }
+
+    private int Dfs(TreeNode root, int prevSum)
+    {
+        if (root == null)
+            return 0;
+
+        var sum = prevSum * 10 + root.val;
+        if (root.left == null && root.right == null)
+        {
+            return sum;
+        }
+
+        return Dfs(root.left, sum) + Dfs(root.right, sum);
+    }
+}
