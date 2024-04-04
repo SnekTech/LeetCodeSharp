3f2c03cd984c5d2e8494b06c73081d8501b7b251
syf <2654525303@qq.com>
2024-04-04 17:05:56 +0800

    112

---
+++ Interview150/112/Solution.cs
@@ -0,0 +1,19 @@
+﻿using Interview150.common;
+
+namespace Interview150._112;
+
+public class Solution {
+    public bool HasPathSum(TreeNode root, int targetSum)
+    {
+        if (root == null)
+            return false;
+
+        if (root.left == null && root.right == null)
+        {
+            return root.val == targetSum;
+        }
+
+        targetSum -= root.val;
+        return HasPathSum(root.left, targetSum) || HasPathSum(root.right, targetSum);
+    }
+}
