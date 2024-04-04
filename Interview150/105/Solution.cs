ba9e91d4934eb72271270f0f1b353c0094a1848c
syf <2654525303@qq.com>
2024-04-04 16:15:53 +0800

    105

---
+++ Interview150/105/Solution.cs
@@ -0,0 +1,27 @@
+﻿using Interview150.common;
+
+namespace Interview150._105;
+
+public class Solution
+{
+    public TreeNode BuildTree(int[] preorder, int[] inorder)
+    {
+        if (preorder.Length == 0)
+            return null;
+
+        var rootVal = preorder[0];
+        var rootIndex = Array.IndexOf(inorder, rootVal);
+        var leftLength = rootIndex;
+        
+        var leftPreorder = preorder[1..(1 + leftLength)];
+        var leftInorder = inorder[..rootIndex];
+        var rightPreorder = preorder[(1 + leftLength)..];
+        var rightInorder = inorder[(rootIndex + 1)..];
+
+        var leftTree = BuildTree(leftPreorder, leftInorder);
+        var rightTree = BuildTree(rightPreorder, rightInorder);
+
+        var root = new TreeNode(rootVal, leftTree, rightTree);
+        return root;
+    }
+}
