0f1459cdefaa5f29ecefb351926423cc55cdb74b
syf <2654525303@qq.com>
2024-04-05 12:52:30 +0800

    139

---
+++ Interview150/DP/139/Solution.cs
@@ -0,0 +1,38 @@
+﻿namespace Interview150.DP._139;
+
+public class Solution
+{
+    public bool WordBreak(string s, IList<string> wordDict)
+    {
+        var n = s.Length;
+        var dp = new bool[n];
+        for (var i = 0; i < n; i++)
+        {
+            var isBreakable = false;
+            foreach (var word in wordDict)
+            {
+                if (i < word.Length - 1)
+                {
+                    continue;
+                }
+                else if (i == word.Length - 1)
+                {
+                    isBreakable |= s[..(i + 1)].Equals(word);
+                }
+                else
+                {
+                    isBreakable |= dp[i - word.Length] && s[(i - word.Length + 1)..(i + 1)].Equals(word);
+                }
+
+                if (isBreakable)
+                {
+                    break;
+                }
+            }
+
+            dp[i] = isBreakable;
+        }
+
+        return dp[n - 1];
+    }
+}
