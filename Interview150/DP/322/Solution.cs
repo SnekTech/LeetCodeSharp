97471a466f80161b3eaecf1eda8d99fc5dc91f14
syf <2654525303@qq.com>
2024-04-05 17:55:15 +0800

    322

---
+++ Interview150/DP/322/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace Interview150.DP._322;
+
+public class Solution
+{
+    public int CoinChange(int[] coins, int amount)
+    {
+        var max = amount + 1;
+        var dp = new int[amount + 1];
+        Array.Fill(dp, max);
+        dp[0] = 0;
+        for (var i = 1; i <= amount; i++)
+        {
+            foreach (var coin in coins)
+            {
+                if (coin <= i)
+                {
+                    dp[i] = Math.Min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
