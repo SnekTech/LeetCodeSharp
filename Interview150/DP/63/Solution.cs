3e9cc2485b9da004c2974e8ee3335514a6acd8ce
syf <2654525303@qq.com>
2024-04-08 09:28:04 +0800

    63

---
+++ Interview150/DP/63/Solution.cs
@@ -0,0 +1,43 @@
+﻿namespace Interview150.DP._63;
+
+public class Solution
+{
+    public int UniquePathsWithObstacles(int[][] obstacleGrid)
+    {
+        int m = obstacleGrid.Length, n = obstacleGrid[0].Length;
+        var dp = new int[m, n];
+        dp[0, 0] = obstacleGrid[0][0] == 1 ? 0 : 1;
+
+        for (var i = 1; i < m; i++)
+        {
+            if (obstacleGrid[i][0] != 0 || dp[i - 1, 0] == 0)
+                break;
+
+            dp[i, 0] = 1;
+        }
+
+        for (var j = 1; j < n; j++)
+        {
+            if (obstacleGrid[0][j] != 0 || dp[0, j - 1] == 0)
+                break;
+
+            dp[0, j] = 1;
+        }
+
+        for (var i = 1; i < m; i++)
+        {
+            for (var j = 1; j < n; j++)
+            {
+                if (obstacleGrid[i][j] != 0)
+                {
+                    dp[i, j] = 0;
+                    continue;
+                }
+
+                dp[i, j] = dp[i - 1, j] + dp[i, j - 1];
+            }
+        }
+
+        return dp[m - 1, n - 1];
+    }
+}
---
+++ Interview150/DP/63/Test.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Interview150.DP._63;
+
+[TestFixture]
+public class Test
+{
+    [Test]
+    public void Case1()
+    {
+        int[][] obstacleGrid = [[1, 0]];
+        var solution = new Solution();
+
+        solution.UniquePathsWithObstacles(obstacleGrid)
+            .Should().Be(0);
+    }
+}
