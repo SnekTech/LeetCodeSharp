921a754975d3cc7e5b4a16bdb90851900a98254f
syf <2654525303@qq.com>
2024-04-16 10:56:54 +0800

    35

---
+++ Interview150/BinarySearch/35/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace Interview150.BinarySearch._35;
+
+public class Solution
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        var n = nums.Length;
+        int left = 0, right = n - 1;
+        while (left <= right)
+        {
+            var mid = left + (right - left) / 2;
+            var midVal = nums[mid];
+            if (target == midVal)
+                return mid;
+            
+            if (target < midVal)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+        
+        return left;
+    }
+}
---
+++ Interview150/BinarySearch/35/Test.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Interview150.BinarySearch._35;
+
+[TestFixture]
+public class Test
+{
+    private Solution _solution = new();
+    
+    [Test]
+    public void Case1()
+    {
+        int[] nums = [1, 3, 5, 6];
+        const int target = 2;
+        var position = _solution.SearchInsert(nums, target);
+
+        position.Should().Be(1);
+    }
+}
